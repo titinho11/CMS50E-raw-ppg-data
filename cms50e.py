6afdd677d5968f3b1dcb3250b62db322207265f0
titinho11 <kateu.firmin@yahoo.fr>
2020-05-15 13:29:05 +0200

    SpO2 value added & timer for start recording

--- cms50e.py
+++ cms50e.py
@@ -2,6 +2,10 @@ import sys
 import serial
 import calendar;
 import time;
+import signal
+import sys
+import os
+import threading
 
 #INPUT :
 #    sys.argv[1] = the serial port of CMS50E device
@@ -10,6 +14,67 @@ import time;
 #OUTPUT :
 # nothing, just writtingg into sys.argv[2] file 
 
+
+cmd_querry = b'\x7d\x81\xa1\x80\x80\x80\x80\x80\x80' #it seem that this is a request live data like command 
+
+from threading import Timer
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+def signal_handler(sig, frame):
+    tf = calendar.timegm(time.gmtime())
+    print(tf-ts, ' Secondes recorded !')
+    global rt
+    global file
+    rt.stop()
+    with open(file, "a") as myfile:
+        myfile.write(str(tf))
+    sys.exit(0)
+
+def maintain():
+    #threading.Timer(54.0, maintain).start()
+    serial.write(cmd_querry)
+
+def delaistart():
+    print("Start recording in...")
+    print(5)
+    time.sleep(1)
+    print(4)
+    time.sleep(1)
+    print(3)
+    time.sleep(1)
+    print(2)
+    time.sleep(1)
+    print(1)
+    time.sleep(1)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+if os.path.exists(sys.argv[2]):
+    os.remove(sys.argv[2])
 serial = serial.Serial(sys.argv[1],
                                     baudrate=115200, #in my device, this is the baudrate where to open the serial, in other devices try 4800, 19200...
                                     timeout=2,
@@ -18,13 +83,19 @@ serial = serial.Serial(sys.argv[1],
 				    stopbits=serial.STOPBITS_ONE,
                                     parity=serial.PARITY_NONE) #another important looking like parameter, setting the parity to NONE
                                     
-serial.write(b'\x7d\x81\xa1\x80\x80\x80\x80\x80\x80') #it seem that this is a request live data like command 
+#serial.write(cmd_querry)) 
+#temporiser avec compteur
+delaistart()
+maintain()
+file = sys.argv[2]
+rt = RepeatedTimer(54, maintain) #send cmd_maintain every 54sec to maintain the stream session
 print("Recording...\n^C to stop.")
 
 ts = calendar.timegm(time.gmtime()) #10 caracteres a echapper dans le decodage de data
 with open(sys.argv[2], "a") as myfile:
         myfile.write(str(ts))
 while True:
-    data = serial.read() #read each byte
+    data = serial.read(9) #read each byte
+    print(data)
     with open(sys.argv[2], "a") as myfile:
         myfile.write(data.hex())
--- decod.py
+++ decod.py
@@ -14,6 +14,7 @@ f.close()
 
 timestamp = data[:10] #retrieving timstamp
 data = data[10:]
+data = ''.join(data.split())[:-10]
 
 print('Start Datetime : ', timestamp)
 
@@ -34,13 +35,28 @@ for dat in dataT:
 
         #first byte
 	byte0 = bin(int(byteTHex[0], 16))[2:].zfill(8)
-	
-	#second byte (PPG Waveform data)
-	byte1 = bin(int(byteTHex[3], 16))[2:].zfill(8) #this is the byte which get the ppg waveform value
-	val = int(str(byte1)[:8],2) #ppg value seems to be the entire second byte
-	print(count,' 2nd byte ', byte1, ' => ', val) 
-	ppgVal.append(val)
+	   #second byte
+	byte1 = bin(int(byteTHex[1], 16))[2:].zfill(8)
+	#third byte
+	byte2 = bin(int(byteTHex[2], 16))[2:].zfill(8)
+	#fifth byte
+	byte4 = bin(int(byteTHex[4], 16))[2:].zfill(8)
+	#sixth byte
+	byte5 = bin(int(byteTHex[5], 16))[2:].zfill(8)
+	#seventh byte - 1:8 
+	byte6 = bin(int(byteTHex[6], 16))[2:].zfill(8)
+	val2 = int(str(byte6)[1:8],2) #SpO2 value seems to be the 0-6 bits of this byte
+
+
+	#fourth byte (PPG Waveform data)
+	byte3 = bin(int(byteTHex[3], 16))[2:].zfill(8) #this is the byte which get the ppg waveform value
+	val = int(str(byte3)[:8],2) #ppg value seems to be the entire second byte
+	#print(count,' 2nd byte ', byte3, ' => ', val) 
+	ppgVal.append(str(val)+'-'+str(val2))
 	count=count+1
+	#print( str(byte4)[0:],'//', str(byte4)[1:],'//', str(byte4)[1:8],'//', str(byte4)[1:7],'//', str(byte4)[0:7]) #array[a:b], return element from the index a to the b-th element
+	#print(int(str(byte0)[1:],2), '/', int(str(byte1)[1:],2), '/', int(str(byte2)[1:],2), '/', int(str(byte3)[0:],2), '/', int(str(byte4)[1:],2), '/', int(str(byte5)[1:],2), '/', int(str(byte6)[1:],2))
+	
 
 #print(ppgVal)
 f = open(sys.argv[2], "w") #writing in the output file the raw ppg data decoded, line by line
